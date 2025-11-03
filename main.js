5745d12a567c68cd7a5a407d433bb7870a55b8a1
Bhavik Jain <125796748+TheBhavikJain@users.noreply.github.com>
2025-11-03 07:15:58 +0000

    Added Friends pop-up.

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 
 function popup() {
-    var cardPopUp = document.getElementById("event_cards");
-    cardPopUp.classList.toggle("show");
+    document.getElementById("eventCard").classList.toggle("show");
 }
+
+// friends code START
+
+const btn = document.getElementById('friends_button');
+const panel = document.getElementById('friends_panel');
+
+btn.addEventListener('click', () => panel.classList.toggle('open'));
+
+document.addEventListener('click', (e) => {
+    const clickedInsidePanel = panel.contains(e.target);
+    const clickedButton = btn.contains(e.target);
+
+    if (panel.classList.contains('open') && !clickedInsidePanel && !clickedButton)
+    {
+        panel.classList.remove('open');
+    }
+})
+
+// friends code END
